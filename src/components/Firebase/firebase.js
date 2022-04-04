62b9a2100dfc7fb56ff6fafb8447feb67ef0646b
Joan Subirats Llaveria <subirats345@gmail.com>
2022-04-04 14:47:40 +0200

    Fixed process .env

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,7 +13,23 @@ const config = {
 class Firebase {
   constructor() {
     initializeApp(config);
+    this.auth = getAuth();
   }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import * as ROUTES from "../../constants/routes";
 
 const SignUp = () => (
   <div>
     <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
 
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onSubmit = (event) => {};
+
+  onChange = (event) => {};
+
+  render() {
+    return <form onSubmit={this.onSubmit}></form>;
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
 export default SignUp;
+
+export { SignUpForm, SignUpLink };
