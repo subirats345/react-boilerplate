d46d999ee0b5a0ea0a8ea375f9eb5ba016492ff2
Joan Subirats Llaveria <subirats345@gmail.com>
2022-04-04 11:34:56 +0200

    basic navBar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import * as ROUTES from '../../constants/routes';
+import * as ROUTES from "../../constants/routes";
 
 const Navigation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
+  <div className="p-2 shadow-md flex justify-between px-6">
+    <div className="text-2xl">Title</div>
+    <ul className="flex">
+      {navigationItems.map((e) => (
+        <li className="h-8 mx-6 hover:bg-slate-200 rounded-md" key={e.link}>
+          <Link to={e.link}>{e.title}</Link>
+        </li>
+      ))}
     </ul>
   </div>
 );
+
+const navigationItems = [
+  {
+    title: "Sign In",
+    link: ROUTES.SIGN_IN,
+  },
+  {
+    title: "Landing",
+    link: ROUTES.LANDING,
+  },
+  {
+    title: "Home",
+    link: ROUTES.HOME,
+  },
+  {
+    title: "Account",
+    link: ROUTES.ACCOUNT,
+  },
+  {
+    title: "Admin",
+    link: ROUTES.ADMIN,
+  },
+];
 
 export default Navigation;
