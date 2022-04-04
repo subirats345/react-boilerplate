3d99c6079630b24d93adc1a7524266f701058e08
Joan Subirats Llaveria <subirats345@gmail.com>
2022-04-04 11:07:00 +0200

    Upgraded to React 18

---
+++ src/components/App/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navigation from "../Navigation";
+import Landing from "../Landing";
+import SignUp from "../SignUp";
+import SignIn from "../SignIn";
+import PasswordForget from "../PasswordForget";
+import Home from "../Home";
+import Account from "../Account";
+import Admin from "../Admin";
+
+import * as ROUTES from "../../constants/routes";
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Routes>
+        <Route exact path={ROUTES.LANDING} component={Landing} />
+        <Route path={ROUTES.SIGN_UP} component={SignUp} />
+        <Route path={ROUTES.SIGN_IN} component={SignIn} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+        <Route path={ROUTES.HOME} component={Home} />
+        <Route path={ROUTES.ACCOUNT} component={Account} />
+        <Route path={ROUTES.ADMIN} component={Admin} />
+      </Routes>
+    </div>
+  </Router>
+);
+
+export default App;
