c63238241251a6e7988b94c76919ac147804a4ac
Joan Subirats Llaveria <subirats345@gmail.com>
2022-04-04 14:15:41 +0200

    Firebase context provider & Firebase new version modular methods

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
-const container =  document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
+  </React.StrictMode>
 );
