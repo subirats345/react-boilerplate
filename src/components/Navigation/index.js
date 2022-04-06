83d06296ed8030b93b5665cac1e31bba7d0ed66f
Joan Subirats Llaveria <subirats345@gmail.com>
2022-04-06 15:26:49 +0200

    NavBar with DaisyUI

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,20 +5,52 @@ import SignOutButton from "../SignOut";
 import * as ROUTES from "../../constants/routes";
 
 const Navigation = () => (
-  <div className="p-2 shadow-md flex justify-between px-6">
-    <div className="text-2xl">Title</div>
-    <ul className="flex">
-      {navigationItems.map((e) => (
-        <li className="h-8 mx-6 hover:bg-slate-200 rounded-md" key={e.link}>
-          <Link to={e.link}>{e.title}</Link>
-        </li>
-      ))}
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
+  <div className="navbar bg-base-100">
+    <div className="navbar-start">
+      <div className="dropdown">
+        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+        <ul
+          tabIndex="0"
+          className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <MenuItems />
+        </ul>
+      </div>
+      <Link to={ROUTES.HOME} className="btn btn-ghost normal-case text-xl">
+        Title
+      </Link>
+    </div>
+    <div className="navbar-center hidden lg:flex">
+      <ul className="menu menu-horizontal p-0">
+        <MenuItems />
+      </ul>
+    </div>
+    <div className="navbar-end">
+      <SignOutButton />
+    </div>
   </div>
 );
+
+const MenuItems = () => {
+  return navigationItems.map((e) => (
+    <li key={e.link}>
+      <Link to={e.link}>{e.title}</Link>
+    </li>
+  ));
+};
 
 const navigationItems = [
   {
--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -3,7 +3,7 @@ import { withFirebase } from "../Firebase";
 
 const SignOutButton = ({ firebase }) => (
   <div>
-    <button type="button" onClick={firebase.doSignOut}>
+    <button type="button" className="btn" onClick={firebase.doSignOut}>
       Sign Out
     </button>
   </div>
