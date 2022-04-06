a49dce43bc9b43c81f31f2d9643d1ee35da1c8fe
Joan Subirats Llaveria <subirats345@gmail.com>
2022-04-06 15:34:23 +0200

    container class added on App Component
    First iteration of a very basic layout system

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -16,17 +16,17 @@ const App = () => (
   <Router>
     <div>
       <Navigation />
-
-      <hr />
-      <Routes>
-        <Route exact path={ROUTES.LANDING} element={<Landing />} />
-        <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-        <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
-        <Route path={ROUTES.PASSWORD_FORGET} element={<PasswordForget />} />
-        <Route path={ROUTES.HOME} element={<Home />} />
-        <Route path={ROUTES.ACCOUNT} element={<Account />} />
-        <Route path={ROUTES.ADMIN} element={<Admin />} />
-      </Routes>
+      <div className="container mx-auto py-4">
+        <Routes>
+          <Route exact path={ROUTES.LANDING} element={<Landing />} />
+          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+          <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+          <Route path={ROUTES.PASSWORD_FORGET} element={<PasswordForget />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.ACCOUNT} element={<Account />} />
+          <Route path={ROUTES.ADMIN} element={<Admin />} />
+        </Routes>
+      </div>
     </div>
   </Router>
 );
