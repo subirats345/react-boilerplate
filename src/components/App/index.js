502331199a50d11430ba491df7ebbaefaf7f8099
Joan Subirats Llaveria <subirats345@gmail.com>
2022-04-07 14:30:01 +0200

    Auth Context

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { withFirebase } from "../Firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { AuthUserContext } from "../Session/index";
 
 import Navigation from "../Navigation";
 import Landing from "../Landing";
@@ -33,22 +34,24 @@ const App = (props) => {
   });
 
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navigation authUser={authUser} />
-        <Routes>
-          <Route exact path={ROUTES.LANDING} element={<Landing />} />
-          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-          <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
-          <Route path={ROUTES.PASSWORD_FORGET} element={<PasswordForget />} />
-          <Route path={ROUTES.HOME} element={<Home />} />
-          <Route path={ROUTES.ACCOUNT} element={<Account />} />
-          <Route path={ROUTES.ADMIN} element={<Admin />} />
-        </Routes>
-        <InfoModal />
-        <Footer />
-      </div>
-    </Router>
+    <AuthUserContext.Provider value={authUser}>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navigation />
+          <Routes>
+            <Route exact path={ROUTES.LANDING} element={<Landing />} />
+            <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+            <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+            <Route path={ROUTES.PASSWORD_FORGET} element={<PasswordForget />} />
+            <Route path={ROUTES.HOME} element={<Home />} />
+            <Route path={ROUTES.ACCOUNT} element={<Account />} />
+            <Route path={ROUTES.ADMIN} element={<Admin />} />
+          </Routes>
+          <InfoModal />
+          <Footer />
+        </div>
+      </Router>
+    </AuthUserContext.Provider>
   );
 };
 
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,82 +1,78 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SignOutButton from "../SignOut";
+import { AuthUserContext } from "../Session";
 
 import * as ROUTES from "../../constants/routes";
 import ThemeToggler from "../Core/ThemeToggler/ThemeToggler";
+import { DropDownIcon } from "../../constants/icons";
 
-const Navigation = ({ authUser }) => {
-  const MenuItems = () => {
-    const navList = authUser ? navigationItemsAuth : navigationItemsNoAuth;
-
-    return navList.map((e) => (
-      <li key={e.link}>
-        <Link to={e.link}>{e.title}</Link>
-      </li>
-    ));
-  };
+const Navigation = () => {
   return (
     // TODO: apply shadow only on scroll
-    <div className="navbar bg-base-100 shadow-md">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex="0" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex="0"
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <MenuItems />
-          </ul>
-        </div>
-        <Link
-          to={authUser ? ROUTES.HOME : ROUTES.LANDING}
-          className="btn btn-ghost normal-case text-xl">
-          Title
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal p-0">
-          <MenuItems />
-        </ul>
-      </div>
-      {/* NavBar End  */}
-      <div className="navbar-end gap-2">
-        <ThemeToggler />
-        {authUser ? (
-          <div className="dropdown dropdown-end">
-            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img alt="profile" src={authUser.photoURL} />
-              </div>
-            </label>
-            <ul
-              tabIndex="0"
-              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-              <li>
-                <Link to={ROUTES.ACCOUNT}>Profile</Link>
-              </li>
-              <li>
-                <SignOutButton />
-              </li>
+    <AuthUserContext.Consumer>
+      {(authUser) => (
+        <div className="navbar bg-base-100 shadow-md">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                {DropDownIcon}
+              </label>
+              <ul
+                tabIndex="0"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <MenuItems isAuth={authUser} />
+              </ul>
+            </div>
+            <Link
+              to={authUser ? ROUTES.HOME : ROUTES.LANDING}
+              className="btn btn-ghost normal-case text-xl">
+              Title
+            </Link>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              <MenuItems isAuth={authUser} />
             </ul>
           </div>
-        ) : null}
-      </div>
-    </div>
+          {/* NavBar End  */}
+          <div className="navbar-end gap-2">
+            <ThemeToggler />
+            {authUser ? (
+              <div className="dropdown dropdown-end">
+                <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img alt="profile" src={authUser.photoURL} />
+                  </div>
+                </label>
+                <ul
+                  tabIndex="0"
+                  className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                  <li>
+                    <Link to={ROUTES.ACCOUNT}>Profile</Link>
+                  </li>
+                  <li>
+                    <SignOutButton />
+                  </li>
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      )}
+    </AuthUserContext.Consumer>
   );
+};
+
+const MenuItems = ({ isAuth }) => {
+  const navList = isAuth ? navigationItemsAuth : navigationItemsNoAuth;
+  console.log(isAuth);
+
+  return navList.map((e) => (
+    <li key={e.link}>
+      <Link to={e.link}>{e.title}</Link>
+    </li>
+  ));
 };
 
 const navigationItemsAuth = [
