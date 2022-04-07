32316801061d97705535f04571f454238e487dec
Joan Subirats Llaveria <subirats345@gmail.com>
2022-04-07 16:45:03 +0200

    Admin and home page design updated

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 
 const Admin = () => (
-  <div className="flex-grow">
-    <h1>Admin</h1>
+  <div className="hero flex-grow bg-base-200">
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">Admin</h1>
+        <p className="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+      </div>
+    </div>
   </div>
 );
 
